8a03a8b5edf4a9b87d4af0dca5708e74bce18267
Luiz Sócrate <luiz@socrate.com.br>
2015-06-09 17:12:37 -0300

    Use pods

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'scielo-helper',
+    podModulePrefix: 'scielo-helper/pods',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
